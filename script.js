604d6341943e4ec95a745cd7da2d44ea48c626f0
gurrium <gurrium@hatena.ne.jp>
2022-06-26 22:19:38 +0900

    v1.0

--- script.js
+++ script.js
@@ -1,69 +1,193 @@
-var myChart
-const fileSelector = document.getElementById('route-file')
-fileSelector.addEventListener('change', (event) => {
+var data = null
+const parser = new DOMParser()
+const chart = createChart()
+
+document.getElementById('route-file').addEventListener('change', event => {
   const fileList = event.target.files;
-  fileList[0].text().then(value => {
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(value, 'text/xml')
-    const trackpointTags = [...doc.getElementsByTagName('Trackpoint')]
-    const data = trackpointTags
-      .map(tag => {
-        const distanceTag = tag.querySelector('DistanceMeters')
-        const altitudeTag = tag.querySelector('AltitudeMeters')
-        if (distanceTag == null || altitudeTag == null) {
-          return null
-        }
-
-        const distance = parseFloat(distanceTag.textContent)
-        const altitude = parseFloat(altitudeTag.textContent)
-        if (isNaN(altitude) || isNaN(distance)) {
-          return null
-        }
-
-        return {
-          x: distance,
-          y: altitude
-        }
-      })
-      .filter(e => e)
-
-    createChart(data)
+  fileList[0].text().then(content => {
+    data = parseData(content)
+    updateChart(data)
   })
-});
+})
 
-function createChart(data) {
-  const ctx = document.getElementById('chart').getContext('2d');
-  myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      datasets: [{
-        data: data,
-        fill: true,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-      }]
-    },
-    options: {
-      scales: {
-        x: {
-          type: 'linear',
-          min: 0,
-        },
-        y: {
-          type: 'linear',
-          min: 0,
-        }
-      },
-      pointRadius: 0,
+document.getElementById('maxDistance').addEventListener('input', event => {
+  debounce(_ => {
+    const parsed = parseFloat(event.target.value)
+    if (isNaN(parsed)) {
+      maxDistance = null
+    } else {
+      maxDistance = parsed
+      updateMaximumMinDistance(maxDistance)
     }
+
+    updateChart(croppedData(minDistance, maxDistance))
+  }, 500)
+})
+
+document.getElementById('minDistance').addEventListener('input', event => {
+  debounce(_ => {
+    const parsed = parseFloat(event.target.value)
+    if (isNaN(parsed)) {
+      minDistance = null
+    } else {
+      minDistance = parsed
+      updateMinimumMaxDistance(minDistance)
+    }
+
+    updateChart(croppedData(minDistance, maxDistance))
+  }, 500)
+})
+
+document.getElementById('maxAltitude').addEventListener('input', event => {
+  debounce(_ => {
+    const parsed = parseFloat(event.target.value)
+    if (isNaN(parsed)) {
+      chart.options.scales.y.max = null
+    } else {
+      chart.options.scales.y.max = parsed
+    }
+
+    chart.update()
+  }, 500)
+})
+
+var timerID
+function debounce(func, delay) {
+  clearTimeout(timerID)
+
+  timerID = setTimeout(func, delay)
+}
+
+function parseData(content) {
+  const doc = parser.parseFromString(content, 'text/xml')
+  const trackpointTags = [...doc.getElementsByTagName('Trackpoint')]
+  return trackpointTags
+    .map((tag, index) => {
+      // if (index % 10 != 0) { return null }
+
+      const distanceTag = tag.querySelector('DistanceMeters')
+      const altitudeTag = tag.querySelector('AltitudeMeters')
+      if (distanceTag == null || altitudeTag == null) {
+        return null
+      }
+
+      const distance = parseFloat(distanceTag.textContent)
+      const altitude = parseFloat(altitudeTag.textContent)
+      if (isNaN(altitude) || isNaN(distance)) {
+        return null
+      }
+
+      return {
+        x: distance / 1000,
+        y: altitude
+      }
+    })
+    .filter(e => e)
+}
+
+function croppedData(minDistance, maxDistance) {
+  var minIndex = null
+  var maxIndex = null
+
+  if (!(minDistance == null)) {
+    minIndex = data.findIndex(e => e.x >= minDistance)
+
+    if (minIndex == -1) {
+      minIndex = null
+    }
+  }
+
+  if (!(maxDistance == null)) {
+    maxIndex = data.findIndex(e => e.x >= maxDistance)
+
+    if (maxIndex == -1) {
+      maxIndex = null
+    } else {
+      maxIndex += 1
+    }
+  }
+
+  return data.slice(minIndex == null ? 0 : minIndex, maxIndex == null ? data.length : maxIndex)
+}
+
+function createChart() {
+  const ctx = document.getElementById('chart').getContext('2d');
+  const chartData = {
+    datasets: [{
+      data: [],
+      fill: true,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+    }]
+  }
+  const options = {
+    scales: {
+      x: {
+        type: 'linear',
+        min: 0,
+        title: {
+          display: true,
+          text: '距離[km]'
+        },
+      },
+      y: {
+        type: 'linear',
+        min: 0,
+        title: {
+          display: true,
+          text: '標高[m]'
+        },
+      }
+    },
+    pointRadius: 0,
+    pointHitRadius: 2,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    // events: ['mousedown', 'mousemove', 'mouseup'],
+    // onHover: e => console.log('hover:', e),
+    animation: {
+      onComplete: _ => {
+        setDownloadableImage()
+      }
+    }
+  }
+
+  return new Chart(ctx, {
+    type: 'line',
+    data: chartData,
+    options: options,
   })
 }
 
-function downloadImage() {
-  const downloadButton = document.getElementById('download-button')
-  downloadButton.href = myChart.toBase64Image()
-  downloadButton.download = `altitudes-${(new Date()).toISOString().split("T")[0]}.png`
-  // downloadButton.click()
-  // TODO: これ動きが自明じゃない気がする
+function updateChart(updatedData) {
+  chart.data.datasets[0].data = updatedData
+  chart.options.scales.x.min = updatedData[0].x
+  chart.options.scales.x.max = updatedData[updatedData.length - 1].x
+  chart.update()
 }
 
+function setDownloadableImage() {
+  const downloadButton = document.getElementById('download-button')
+
+  if (chart.data.datasets[0].data.length <= 1) {
+    downloadButton.classList.add('pure-button-disabled')
+    return
+  }
+
+  downloadButton.classList.remove('pure-button-disabled')
+  downloadButton.href = chart.toBase64Image()
+  downloadButton.download = `altitudes-${(new Date()).toISOString().split("T")[0]}.png`
+}
+
+function updateMaximumMinDistance(maxDistance) {
+  const minDistanceInput = document.getElementById('minDistance')
+  minDistanceInput.max = maxDistance
+}
+
+function updateMinimumMaxDistance(minDistance) {
+  const maxDistanceInput = document.getElementById('maxDistance')
+  maxDistanceInput.min = minDistance
+}
