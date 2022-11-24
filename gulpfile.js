8d299f5597167a0fd26b84585a5d8b4ed374b764
gurrium <gurrium@hatena.ne.jp>
2022-11-24 21:22:53 +0900

    add terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,33 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
-var watchify = require("watchify");
+var terser = require("gulp-terser");
 var tsify = require("tsify");
-var fancy_log = require("fancy-log");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
 var paths = {
   pages: ["src/*.html"],
 };
-var watchedBrowserify = watchify(
-  browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["src/script.ts"],
-    cache: {},
-    packageCache: {},
-  }).plugin(tsify)
-);
 gulp.task("copy-html", function () {
   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
-function bundle() {
-  return watchedBrowserify
-    .bundle()
-    .on("error", fancy_log)
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
-}
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/script.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(terser())
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("dist"));
+  })
+);
